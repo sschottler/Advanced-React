7d5a23ee7287586b27d406b92074f15f5c64a526
Scott Schottler <scottschottler@gmail.com>
2018-12-28 11:19:36 -0500

    41 - Displaying Cart Items and Totals

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
+import { CURRENT_USER_QUERY } from './User'
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -15,11 +16,16 @@ class AddToCart extends React.Component {
     const { id } = this.props
     return (
       <Mutation
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
         mutation={ADD_TO_CART_MUTATION}
         variables={{
           id
         }}>
-        {addToCart => <button onClick={addToCart}>Add To Cart 🛒</button>}
+        {(addToCart, { loading }) => (
+          <button disabled={loading} onClick={addToCart}>
+            Add{loading && 'ing'} To Cart 🛒
+          </button>
+        )}
       </Mutation>
     )
   }
