5712167fd69b686a18f99623afdfa8a1945da4bd
Scott Schottler <scottschottler@gmail.com>
2018-12-28 15:32:31 -0500

    53 - Orders Page
    Module #8 done. Only Testing and deployment left

---
+++ sick-fits/frontend/pages/orders.js
@@ -0,0 +1,11 @@
+import PleaseSignIn from '../components/PleaseSignIn'
+import OrderList from '../components/OrderList'
+
+const Orders = props => (
+  <div>
+    <PleaseSignIn>
+      <OrderList />
+    </PleaseSignIn>
+  </div>
+)
+export default Orders
